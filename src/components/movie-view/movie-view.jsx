00cbc67b46bf8cb1be4dcb2e80481cf400005dbf
Aneri3112 <aneri.pate@hotmail.com>
2022-05-03 22:38:06 +0200

    Import Prop-types

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieView extends React.Component {
 
@@ -20,11 +21,27 @@ export class MovieView extends React.Component {
         </div>
         <div className="movie-genre">
           <span className="label">Genre: </span>
-          <span className="value">{movie.Genre}</span>
-        </div>
+          <span className="value">{movie.Genre.Name}</span>
+         </div>
+         <div className="genre-description">
+          <span className="genre">Description: </span>
+          <span className="value">{movie.Genre.Description}</span>
+         </div>
         <div className="movie-director">
           <span className="label">Director: </span>
-          <span className="value">{movie.Director}</span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <div className="director-bio">
+          <span className="director">Bio: </span>
+          <span className="value">{movie.Director.Bio}</span>
+        </div>
+        <div className="director-birth">
+          <span className="director">Birth: </span>
+          <span className="value">{movie.Director.Birth}</span>
+        </div>
+        <div className="director-death">
+          <span className="director">Death: </span>
+          <span className="value">{movie.Director.Death}</span>
         </div>
         <button onClick={() => { onBackClick(null); }}>Back</button>
 
@@ -32,3 +49,22 @@ export class MovieView extends React.Component {
     );
   }
 }
+
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    Imagepath: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string.isRequired,
+      Bio:  PropTypes.string.isRequired,
+      Birth: PropTypes.string.isRequired,
+      Death: PropTypes.string.isRequired
+    })
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired
+};
