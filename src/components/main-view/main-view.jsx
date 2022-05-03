e7c4cf25fd9cbe1bbcc568e3d81a241e85020d07
Aneri3112 <aneri.pate@hotmail.com>
2022-05-03 22:37:20 +0200

    import axios to main-view file
    Imported Axios , RegistrationView and LoginView. Linked herokuapp

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import axios from 'axios';
+
+import { RegistrationView } from '../regigstration-view/registration-view';
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,33 +11,22 @@ export class MainView extends React.Component {
   constructor(){
     super();
     this.state = {
-      movies: [ 
-        { _id: 1, 
-          Title: 'Inception',
-          Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O., but his tragic past may doom the project and his team to disaster.',
-          Genre: 'Sci-Fi',      
-          Director: 'Christopher Nolan',
-          Imagepath: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX67_CR0,0,67,98_AL_.jpg'
-        },
-      
-        { _id: 2, 
-          Title: '3 Idiots',
-          Description: 'Two friends are searching for their long lost companion. They revisit their college days and recall the memories of their friend who inspired them to think differently, even as the rest of the world called them "idiots".',
-          Genre: 'Comedy',
-          Director: 'Rajkumar Hirani',
-          Imagepath: 'https://m.media-amazon.com/images/M/MV5BNTkyOGVjMGEtNmQzZi00NzFlLTlhOWQtODYyMDc2ZGJmYzFhXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UY98_CR0,0,67,98_AL_.jpg'
-        },
-     
-        { _id: 3, 
-          Title: 'Joker',
-          Description: 'A mentally troubled stand-up comedian embarks on a downward spiral that leads to the creation of an iconic villain.',
-          Genre: 'Thriller',
-          Director: 'Todd Phillips',
-          Imagepath: 'https://m.media-amazon.com/images/M/MV5BNGVjNWI4ZGUtNzE0MS00YTJmLWE0ZDctN2ZiYTk2YmI3NTYyXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX67_CR0,0,67,98_AL_.jpg'
-        }
-      ],
-      selectedMovie: null
+      movies: [],
+      selectedMovie: null,
+      user: null
     };
+  }
+
+  componentDidMount() {
+    axios.get('https://movie-api3112.herokuapp.com/movies')
+    .then(response => {
+      this.setState({
+        movies: response.data
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
   setSelectedMovie(newSelectedMovie) {
@@ -42,13 +35,29 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState ({
+      user
+    });
+  }
+
+  onRegistration(register) {
+    this.setState({
+      register
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user, register } = this.state;
+
+    if (!register) return (<RegistrationView onRegistration={(register) => this.onRegistration(register)}/>);
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
 
     //if (selectedMovie) return <MovieView movie={selectedMovie} />;
 
     if (movies.length === 0)
-    return <div className="main-view">The list is empty!</div>;
+    return <div className="main-view" />;
 
     return (
       <div className="main-view">
@@ -62,4 +71,3 @@ export class MainView extends React.Component {
     ); 
   }
 } 
-export default MainView;
