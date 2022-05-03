66a7f458b54d31619b01b1aa1a9486d2bb9b1115
Aneri3112 <aneri.pate@hotmail.com>
2022-05-03 23:05:52 +0200

    Added Register button

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -12,6 +12,11 @@ export function LoginView(props) {
     props.onLoggedIn(username);
   };
 
+  const handleRegister = (e) => {
+    e.preventDefault()
+    props.onRegistration(true)
+  }
+
   return (
     <form>
       <label>
@@ -23,6 +28,7 @@ export function LoginView(props) {
         <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
       </label>
       <button type="submit" onClick={handleSubmit}>Submit</button>
+      <button type='submit' onClick={handleRegister}> Register Your Account</button>
     </form>
   );
 }
