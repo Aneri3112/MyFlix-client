521ff7e7a88a2bad9df7eb5d65ef55c2bf83ea06
Aneri3112 <aneri.pate@hotmail.com>
2022-05-09 18:57:43 +0200

    Added navbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDom from "react-dom";
-import Container from "react-bootstrap/Container";
 import { MainView } from "./components/main-view/main-view"; 
 
 import './index.scss';
+import { Container, Navbar, Nav } from "react-bootstrap";
 
 //Main Component
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container id="main-view">
-        <MainView />
+      <Container>
+        <Navbar id="navbar" collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container fluid>
+            <Navbar.Brand id="navbrand" href="#home">MyFlix</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link id="navhome" href="#home">Home</Nav.Link>
+                <Nav.Link id="navlink" href="#link">Link</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar> <br /> <br />
+        
+          < MainView />
       </Container>
     );
   }
