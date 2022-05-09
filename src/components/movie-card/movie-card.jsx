10edf13c231c4df27014b295c27cad5b4fceee73
Aneri3112 <aneri.pate@hotmail.com>
2022-05-09 19:03:59 +0200

    Fixed syntax error

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { CardGroup, Container, Card, Button, Row, Col } from 'react-bootstrap';
+import { CardGroup, Card, Button } from 'react-bootstrap';
 
 import './movie-card.scss';
 
@@ -8,21 +8,17 @@ export class MovieCard extends React.Component {
   render() {
     const { movie, onMovieClick } = this.props;
     return (
-      <Container className='movie-card-container'>
-        <Row xs={1} md={0} className="g-4"> 
-            <Col>
-              <CardGroup id='movie-cardgroup'>
-              <Card id='movie-card' className='col-sm-4'>
-              <Card.Img id='card-image' variant='top' src={movie.Imagepath} />
-                <Card.Body>
-                  <Card.Title id='card-title'>{movie.Title}</Card.Title>
-                  <Button id='card-button' onClick={() => onMovieClick(movie)} variant='link'> More Detail</Button>
-                </Card.Body>
-              </Card>
-              </CardGroup>
-            </Col>
-        </Row>
-      </Container>  
+      
+      <CardGroup id='movie-cardgroup'>
+        <Card id='movie-card' className='cards'>
+          <Card.Img id='card-image' variant='top' src={movie.Imagepath} />
+          <Card.Body>
+            <Card.Title id='card-title'>{movie.Title}</Card.Title>
+            <Card.Text id='card-description'>{movie.Description}</Card.Text>
+            <Button id='card-button' onClick={() => onMovieClick(movie)} variant='link'> More Detail</Button>
+          </Card.Body>
+        </Card>
+      </CardGroup>
     ) 
   }
 }
@@ -30,6 +26,8 @@ export class MovieCard extends React.Component {
 MovieCard.propType = {
   movie: PropTypes.shape({
     Title: PropTypes.string.isRequired,
-  }).isRequired,
+    Description: PropTypes.string.isRequired,
+    Imagepath: PropTypes.string.isRequired
+  }),
   onMovieClick: PropTypes.func.isRequired
 };
