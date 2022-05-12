9be1960c34b4daa90ff3ce83b29fccf14ec4dbde
Aneri3112 <aneri.pate@hotmail.com>
2022-05-12 23:21:20 +0200

    Attached user & logout link

--- src/components/header-view/header-view.jsx
+++ src/components/header-view/header-view.jsx
@@ -1,22 +1,46 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Container, Navbar, Nav, Button } from "react-bootstrap";
 import './header-view.scss';
 
 export function HeaderView({ user }) {
+
+  const onLoggedOut = () => {
+    localStorage.clear();
+    window.open("/", "_self");
+  }
+
+  const isAuth = () => {
+    if(typeof window == "undefined") {
+      return false;
+    }
+    if(localStorage.getItem("token")) {
+      return localStorage.getItem("token");
+    } else {
+      return false;
+    }
+  };
 
   return (
 
     <Navbar id="navbar" collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container fluid>
-          <Navbar.Brand id="navbrand" href="#home">MyFlix</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link id="navhome" href="#home">Home</Nav.Link>
-              <Nav.Link id="navprofile" href="#profile">Profile</Nav.Link>
-            </Nav>
-            <Nav className="justify-content-end">
-            <Nav.Link id="navlink" href="#link">Logout</Nav.Link>
+          <Navbar.Brand id="navbrand" href="/">MyFlix</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              {isAuth() && (
+                <Nav.Link id="header-link" href={`/users/${user}`}>{user}</Nav.Link>
+              )}
+              {isAuth() && (
+                <Button className="justify-content-end" variant="dark" onClick={() => { onLoggedOut() }}>Logout</Button>
+              )}
+              {!isAuth() && (
+                <Nav.Link id="header-link" href="/">Sign-in</Nav.Link>
+              )}
+              {!isAuth() && (
+                <Nav.Link id="header-link" href="/register">Sign-up</Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
       </Container>
