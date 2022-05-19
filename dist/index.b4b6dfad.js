3e29d4602d5a67d2596fe15ae562e311ce2b337f
Aneri3112 <aneri.pate@hotmail.com>
2022-05-19 14:22:19 +0200

    Auto changes

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -41479,6 +41479,7 @@ function MoviesList(props) {
         children: [
             /*#__PURE__*/ _jsxDevRuntime.jsxDEV(_colDefault.default, {
                 md: 12,
+                xs: 8,
                 style: {
                     margin: '1em'
                 },
